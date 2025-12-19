3c189efef8eeda0b2cefee7085788dc9ecba6890
Joanna <joannashih215@gmail.com>
2025-12-19 22:37:16 +0800

    Implement determinant matrix logic

--- matrix_op.c
+++ matrix_op.c
@@ -38,3 +38,8 @@ void matrix_transpose(float A[SIZE][SIZE], float result[SIZE][SIZE]) {
         }
     }
 }
+float matrix_determinant(float A[SIZE][SIZE]) {
+    return A[0][0] * (A[1][1] * A[2][2] - A[1][2] * A[2][1]) -
+           A[0][1] * (A[1][0] * A[2][2] - A[1][2] * A[2][0]) +
+           A[0][2] * (A[1][0] * A[2][1] - A[1][1] * A[2][0]);
+}
